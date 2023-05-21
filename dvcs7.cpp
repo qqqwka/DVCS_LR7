d4ffc31102b2cb4d734220e2fb441d5b9bf910b0
qqqwka <janiusvg@gmail.com>
2023-05-22 01:23:37 +0300

    10 commit

--- dvcs7.cpp
+++ dvcs7.cpp
@@ -51,7 +51,7 @@ int main(int argc, char* argv[])
 
 	vector<vector<double>> totalCoordinatesVector;
 
-	int numberOfVertices = 0; //количество вершин в многоугольнике
+	int numberOfVertices = 3; //10 commit
 
 	double coordinateItself; //сама координата
 
