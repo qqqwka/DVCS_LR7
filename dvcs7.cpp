c0780915f8dc5306bb114a6db364a1fd3b5490b3
qqqwka <janiusvg@gmail.com>
2023-05-22 01:21:08 +0300

    7 commit

--- dvcs7.cpp
+++ dvcs7.cpp
@@ -11,7 +11,7 @@ int main(int argc, char* argv[])
 	//----------ПРОВЕРКА ОШИБОК РАБОТЫ С ФАЙЛАМИ----------
 
 	//Выдать ошибку, если входной файл не указан в аргументах командной строки
-	if (argc == 2) //1 commit
+	if (argc == 7) //7 commit
 	{
 		cout << "The input data file is specified incorrectly. It is possible that the file at the specified location does not exist.\n";
 		return 6; //5 commit
